f58750dd7ddbec183f6c59895092886661822c78
xiaoshihou <xiaoshihou@tutamail.com>
2026-05-05 13:53:16 +0100

    chore: 添加桌面端启动脚本

---
+++ launch.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+cd /home/xiaoshihou/Playground/scratch/niacg
+kitten @ launch bash -c 'cd /home/xiaoshihou/Playground/scratch/niacg && npm run dev'
+npm run backend
---
+++ launch_kitty.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+kitty -o allow_remote_control=yes -o enabled_layouts=tall /home/xiaoshihou/Playground/scratch/niacg/launch.sh
